ffb508b71e2a595115960bf143df6019aa044a5a
Archambault, Jacob W. (H2 Technology Group, Llc) <Jacob.Archambault@va.gov>
2026-03-15 23:38:14 -0400

    Update dot format so we only display the name, not the path

--- src/dependency_grapher/exporters/dot.py
+++ src/dependency_grapher/exporters/dot.py
@@ -14,10 +14,13 @@ class DotExporter(GraphExporter):
         lines = ["digraph Dependencies {", "  rankdir=LR;"]
 
         for node_key in graph.sorted_nodes():
-            lines.append(f'  "{node_key}";')
+            label = graph.nodes[node_key].path.name.replace('"', r'\"')
+            lines.append(f'  "{label}" ;')
 
         for source, target in graph.edges():
-            lines.append(f'  "{source}" -> "{target}";')
+            source_label = graph.nodes[source].path.name.replace('"', r'\"')
+            target_label = graph.nodes[target].path.name.replace('"', r'\"')
+            lines.append(f'  "{source_label}" -> "{target_label}";')
 
         lines.append("}")
         output_path.write_text("\n".join(lines) + "\n", encoding="utf-8")
